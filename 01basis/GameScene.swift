918aef1f89528a612eb8518aca2c69c2a59e748c
Микола Омельянов <mykola.omelyanov@gmail.com>
2019-07-19 09:10:20 +0300

    asteroid generator

--- 01basis/GameScene.swift
+++ 01basis/GameScene.swift
@@ -12,16 +12,9 @@ import GameplayKit
 class GameScene: SKScene {
     var spaceShip: SKSpriteNode!
     override func didMove(to view: SKView) {
-        let spaceBG = SKSpriteNode(imageNamed: "backgroundsp")
-        spaceBG.size = CGSize(width: UIScreen.main.bounds.width * 3, height: UIScreen.main.bounds.height * 3)
-        addChild(spaceBG)
-        let spaceShipKind = "spaceShips_00" + String(arc4random() % 9 + 1)
-        spaceShip = SKSpriteNode(imageNamed: spaceShipKind)
-        spaceShip.setScale(0.5)
-        spaceShip.position = CGPoint(x: 0, y: 0)
-        spaceShip.physicsBody = SKPhysicsBody(texture: spaceShip.texture!, size: spaceShip.size)
-        spaceShip.physicsBody?.isDynamic = false
-        addChild(spaceShip)
+        setScreen()
+        addSpaceShip()
+        addAsteroids()
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
@@ -33,14 +26,36 @@ class GameScene: SKScene {
     func createAsteroid() -> SKSpriteNode {
         let asteroidKind = "spaceMeteors_00" + String(arc4random() % 4 + 1)
         let asteroid = SKSpriteNode(imageNamed: asteroidKind)
-        asteroid.setScale(0.2)
+        asteroid.setScale(0.15)
         asteroid.position.x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 16))
         asteroid.position.y = frame.size.height + asteroid.size.height
         asteroid.physicsBody = SKPhysicsBody(texture: asteroid.texture!, size: asteroid.size)
         return asteroid
     }
+    func addAsteroids(){
+        let asteroidCreate = SKAction.run {
+            let asteroid = self.createAsteroid()
+            self.addChild(asteroid)
+        }
+        let asteroidCreationInterval = SKAction.wait(forDuration: 1.0, withRange: 0.5)
+        let asteroidSequenceAction = SKAction.sequence([asteroidCreate,asteroidCreationInterval])
+        let asteroidRunAction = SKAction.repeatForever(asteroidSequenceAction)
+        run(asteroidRunAction)
+    }
+    func setScreen(){
+        let spaceBG = SKSpriteNode(imageNamed: "backgroundsp")
+        spaceBG.size = CGSize(width: UIScreen.main.bounds.width * 3, height: UIScreen.main.bounds.height * 3)
+        addChild(spaceBG)
+    }
+    func addSpaceShip(){
+        let spaceShipKind = "spaceShips_00" + String(arc4random() % 9 + 1)
+        spaceShip = SKSpriteNode(imageNamed: spaceShipKind)
+        spaceShip.setScale(0.4)
+        spaceShip.position = CGPoint(x: 0, y: 0)
+        spaceShip.physicsBody = SKPhysicsBody(texture: spaceShip.texture!, size: spaceShip.size)
+        spaceShip.physicsBody?.isDynamic = false
+        addChild(spaceShip)
+    }
     override func update(_ currentTime: TimeInterval) {
-        let asteroid = createAsteroid()
-        addChild(asteroid)
     }
 }
